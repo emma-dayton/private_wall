6b32ee8500a7845892522654f88c52124f23757b
Emma Dayton <emma.dayton85@gmail.com>
2019-03-18 13:40:09 -0700

    create send route for server add database calls for route to insert and dispaly messages, add message displays to wall, update layout navbar and header messages, remove message from index

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 @app.route('/')
 def login_register():
+    if 'user' in session:
+        return redirect('/wall')
     session['title'] = 'Login or Register'
     return render_template('index.html')
 
@@ -16,6 +18,11 @@ def login_register():
 def registration():
     is_valid = True
     db = connectToMySQL('private_wall')
+    session['fn'] = request.form['fn']
+    session['ln'] = request.form['ln']
+    session['email'] =request.form['email']
+    if 'email2' in session: # gets rid of email from login session data
+        session.pop('email2')
     if len(request.form['fn']) < 1:
         flash('Must enter a first name to register', 'fn')
         is_valid = False
@@ -46,6 +53,10 @@ def registration():
                 created_at, updated_at) VALUES(%(fn)s, %(ln)s, %(email)s,
                 %(pw)s, now(), now())"""
         db.query_db(query, data)
+        db = connectToMySQL('private_wall')
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        user = db.query_db(query, data)
+        session['user'] = user[0]
         return redirect('/wall')
     else:
         print("didn't work &&&&&&&&&&&&&&&&&&&&&")
@@ -53,10 +64,17 @@ def registration():
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = {'email': request.form['email']}
+    data = {'email': request.form['email2']}
     query = 'SELECT pw_hash FROM users WHERE email=%(email)s'
     db = connectToMySQL('private_wall')
     check = db.query_db(query, data)
+    session['email2'] = request.form['email2']
+    if 'fn' in session: # clears first name from session in case of registration then login try
+        session.pop('fn')
+    if 'ln' in session: # clears last name from session in case of registration then login try
+        session.pop('ln')
+    if 'email' in session: # clears email from session in case of registration then login try
+        session.pop('email')
     try: # does this block if database query works
         if bcrypt.check_password_hash(check[0]['pw_hash'], request.form['pw']):
             db = connectToMySQL('private_wall')
@@ -76,7 +94,44 @@ def login():
 def wall():
     session['title'] = 'Wall'
     db = connectToMySQL('private_wall')
-    return render_template('wall.html')
+    query = "SELECT * FROM users WHERE NOT id = %(id)s;"
+    users = db.query_db(query, session['user'])
+    print(session['user'],'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    db = connectToMySQL('private_wall')
+    query = "SELECT * FROM messages WHERE receive_user_id=%(id)s;"
+    rec_mess = db.query_db(query, session['user'])
+    db = connectToMySQL('private_wall')
+    query = "SELECT * FROM messages WHERE send_user_id=%(id)s;"
+    sent_mess = db.query_db(query, session['user'])
+    return render_template('wall.html', users=users, rec_mess=rec_mess, sent_mess=sent_mess)
+
+@app.route('/logout')
+def logout():
+    session_keys = []
+    for key in session.keys():
+        session_keys.append(key)
+    for key in session_keys:
+        session.pop(key)
+    return redirect('/')
+
+@app.route('/send', methods=['POST'])
+def send():
+    db = connectToMySQL('private_wall')
+    full_name = request.form['select_friend'].split(' ')
+    receiver = {'first_name':full_name[0], 'last_name': full_name[1]}
+    query = 'SELECT id FROM users WHERE first_name = %(first_name)s AND last_name = %(last_name)s;'
+    rec_id = db.query_db(query, receiver)
+    db = connectToMySQL('private_wall')
+    query = """INSERT INTO messages (content, created_at, updated_at, send_user_id,
+    receive_user_id) VALUES(%(content)s, now(), now(), %(send_id)s, %(rec_id)s)"""
+    data = {
+    'content': request.form['message'],
+    'rec_id':rec_id[0]['id'],
+    'send_id':session['user']['id']
+    }
+    db.query_db(query, data)
+    print(session, '&^&^&^&^&^&^&^&^&^&^&^&^&^&&^&^&^')
+    return redirect('/wall')
 
 
 
